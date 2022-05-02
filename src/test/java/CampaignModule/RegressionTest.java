2b5a9b49690ce389dcea6a8f8390a11ec766c81a
pradeepPandey <pradeep221pwc@gmail.com>
2022-05-02 21:21:32 +0530

    This is for pipeline update.

--- src/test/java/CampaignModule/RegressionTest.java
+++ src/test/java/CampaignModule/RegressionTest.java
@@ -2,7 +2,7 @@ package CampaignModule;
 
 import org.testng.annotations.Test;
 
-public class Regression {
+public class RegressionTest {
 @Test
 public void regressionTest() {
 	System.out.println("This is regression");
