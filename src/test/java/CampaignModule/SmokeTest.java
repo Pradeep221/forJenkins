a8867ce85f64b1427f2343d1942271b9ef94510a
pradeepPandey <pradeep221pwc@gmail.com>
2022-05-02 21:05:52 +0530

    This is pipeline update.

--- src/test/java/CampaignModule/SmokeTest.java
+++ src/test/java/CampaignModule/SmokeTest.java
@@ -1,5 +1,10 @@
 package CampaignModule;
 
-public class SmokeTest {
+import org.testng.annotations.Test;
 
+public class SmokeTest {
+@Test
+public void smokeTest() {
+	System.out.println("This is Smoke testing");
+}
 }
